a3534aa40b4eb7bf85a23834b9b47b9f9e2c0425
ekshej <bakshaj99@gmail.com>
2020-11-12 01:35:04 -0500

    Can change graphs now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import ForceGraph from './graphs/ForceGraph.js';
+import Graph from './graphs/Graph.js';
 
+
+var i = 0;
+var datas = []
 function App() {
+  setUpGraphs();
+  var g = new Graph();
+  const [data, setData] = useState(g);
+
+  useEffect(() => {
+      changeData();
+  }, []);
+
+  const changeData = () => {
+      setData(datas[i++%3]);
+      if(i === datas.length) i = 0;
+  }
+
   return (
     <div className="App">
-      <ForceGraph/>
+      <button onClick={changeData}>Change Data</button>
+      <ForceGraph data={data}/>
     </div>
   );
+}
+
+function setUpGraphs(){
+  var graph1 = new Graph();
+    graph1.addVertex(0);
+    graph1.addVertex(1);
+    graph1.addVertex(2);
+    graph1.addVertex(3);
+    graph1.addVertex(4);
+    graph1.addVertex(5);
+    graph1.addVertex(6);
+
+    graph1.addEdge(0, 1);
+    graph1.addEdge(0, 2);
+    graph1.addEdge(1, 4);
+    graph1.addEdge(2, 3);
+    graph1.addEdge(2, 4);
+    graph1.addEdge(2, 5);
+    graph1.addEdge(3, 6);
+    graph1.addEdge(4, 5);
+
+var graph2 = new Graph();
+    graph2.addVertex(0);
+    graph2.addVertex(1);
+    graph2.addVertex(2);
+    graph2.addVertex(3);
+    graph2.addVertex(4);
+    graph2.addVertex(5);
+    graph2.addVertex(6);
+
+    graph2.addEdge(0, 3);
+    graph2.addEdge(1, 2);
+    graph2.addEdge(1, 4);
+    graph2.addEdge(2, 4);
+    graph2.addEdge(2, 3);
+    graph2.addEdge(2, 5);
+    graph2.addEdge(3, 6);
+
+var graph3 = new Graph();
+    graph3.addVertex(0);
+    graph3.addVertex(1);
+    graph3.addVertex(2);
+    graph3.addVertex(3);
+    graph3.addVertex(4);
+    graph3.addVertex(5);
+    graph3.addVertex(6);
+
+    graph3.addEdge(0, 1);
+    graph3.addEdge(0, 2);
+    graph3.addEdge(1, 5);
+    graph3.addEdge(2, 3);
+    graph3.addEdge(2, 4);
+    graph3.addEdge(2, 5);
+    graph3.addEdge(2, 6);
+    graph3.addEdge(3, 6);
+    graph3.addEdge(4, 2);
+    graph3.addEdge(5, 6);
+
+  datas = [graph1, graph2, graph3];
 }
 
 export default App;
--- src/graphs/ForceGraph.js
+++ src/graphs/ForceGraph.js
@@ -1,34 +1,59 @@
 import * as React from "react";
 import ForceGraph2D from 'react-force-graph-2d';
 
-class ForceGraph extends React.Component {
+class ForceGraph extends React.Component{
+    
     render() {
-        var data = {};
-        data.nodes = [
-                    {"id": "0", "infected": true},
-                    {"id": "1", "infected": false},
-                    {"id": "2", "infected": true},
-                    {"id": "3", "infected": false},
-                    {"id": "4", "infected": false},
-                    {"id": "5", "infected": true},
-                    {"id": "6", "infected": false}
-                ]
-        data.links = [
-                    {"source": "0", "target": "1"},
-                    {"source": "0", "target": "2"},
-                    {"source": "1", "target": "4"},
-                    {"source": "2", "target": "3"},
-                    {"source": "2", "target": "4"},
-                    {"source": "2", "target": "5"},
-                    {"source": "3", "target": "6"},
-                    {"source": "4", "target": "5"}
-                ]
+        var data = setUpData(this.props.data);
+        // data.nodes = [
+        //             {"id": "0", "infected": true},
+        //             {"id": "1", "infected": false},
+        //             {"id": "2", "infected": true},
+        //             {"id": "3", "infected": false},
+        //             {"id": "4", "infected": false},
+        //             {"id": "5", "infected": true},
+        //             {"id": "6", "infected": false}
+        //         ]
+        // data.links = [
+        //             {"source": "0", "target": "1"},
+        //             {"source": "0", "target": "2"},
+        //             {"source": "1", "target": "4"},
+        //             {"source": "2", "target": "3"},
+        //             {"source": "2", "target": "4"},
+        //             {"source": "2", "target": "5"},
+        //             {"source": "3", "target": "6"},
+        //             {"source": "4", "target": "5"}
+        //         ]
         return <ForceGraph2D graphData={data}
                 nodeColor={d => d.infected ? "red" : "green"}
                 linkOpacity={0.5}	
                 linkWidth={3}
                 />;
     }
+
+
   }
+function setUpData(data){
+  // constructing nodes data for simulation
+    var nodes = []
+    for(var i of data.getVertices()){
+        var node = {}
+        node.id = i;
+        node.infected = false;
+        nodes[nodes.length] = node;
+    }
+
+    // constructing links data for simulation
+    var links = []
+    for(var i of data.getVertices()){
+        for(var j of data.getNeighbors(i)){
+            var link = {};
+            link.source = i;
+            link.target = j;
+            links[links.length] = link;
+        }
+    }
+    return { nodes, links };
+}
 
 export default ForceGraph;
