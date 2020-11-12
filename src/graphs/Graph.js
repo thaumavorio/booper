e09de370a6997a30709dcd739a12282894eec37d
Connor Anderson <canderson@thaumavor.io>
2020-11-12 01:33:57 -0500

    Graph bootstrap percolation implementation and calls to greedy/min remotely

--- src/graphs/Graph.js
+++ src/graphs/Graph.js
@@ -1,6 +1,7 @@
 export default class Graph {
     constructor() {
       this.adj = new Map();
+      this.activeVertices = new Set();
     }
   
     addVertex(v) {
@@ -22,5 +23,62 @@ export default class Graph {
   
     getDegree(v) {
       return this.getNeighbors(v).size;
+    }
+
+    activateVertex(v) {
+      return this.activeVertices.add(v);
+    }
+
+    bootstrapPercolationIteration(threshold) {
+      const vertices = this.getVertices();
+
+      for (let v of vertices) {
+        console.log("Looking at", v);
+        if (this.activeVertices.has(v)) {
+          console.log("\tIt's already active.");
+          continue;
+        }
+
+        const neighbors = this.getNeighbors(v);
+
+        let count = 0;
+
+        for (let n of neighbors) {
+          if (this.activeVertices.has(n)) {
+            count++;
+          }
+        }
+
+        console.log("\tIt has", count, "active neighbors.");
+
+        if (threshold <= count) {
+          console.log("\tIt is thus infected.");
+          this.activeVertices.add(v);
+        }
+      }
+    }
+
+    getEdgeString() { // TODO: make not ugly on both Haskell and JS side. Also, factor out to getEdges()
+      var edge = "[";
+      for (let v of this.getVertices()) {
+        for (let n of this.getNeighbors(v)) {
+          if (v < n) { // TODO: assumes simple. is this ok?
+            edge += `(${v},${n}),`
+          }
+        }
+      }
+      return edge.slice(0, -1) + "]";
+    }
+
+    // NB: returns promise
+    findContagiousSetGreedily(threshold) {
+      return fetch(`http://71.87.211.162:13894/greedy?graph=${this.getEdgeString()}&threshold=${threshold}`)
+               .then(res => res.json());
+    }
+
+    // NB: returns promise
+    findMinimalContagiousSet(threshold) {
+      return fetch(`http://71.87.211.162:13894/min?graph=${this.getEdgeString()}&threshold=${threshold}`)
+               .then(res => res.json());
     }
   }
