497b600f852b74d7e0b9ec59ce00944719191181
Henry Poskanzer <hposkanzer@gmail.com>
2020-12-01 00:46:53 -0500

    Uploading a file now replaces the graph with the new graph defined in the file, or it causes a warning to pop up.

--- src/graphs/ForceGraph.js
+++ src/graphs/ForceGraph.js
@@ -29,6 +29,77 @@ class ForceGraph extends React.Component{
         };
     }
 
+    readAdjacencyMatrix = (evt) => {
+        const file = evt.target.files[0];
+        document.getElementById("uploadAdjacencyMatrix").value = null;
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            // Parse input file.
+            const string = event.target.result;
+            const matrix = string.split('\n');
+            for(var i in matrix) {
+                matrix[i] = matrix[i].trim().split(',');
+            }
+            
+            // Check for invalid input.
+            for(var c of matrix[0]) {
+                if(c !== '+' && c !== '-') {
+                    window.alert("Invalid input. There must be a row of +'s and -'s above the adjacency matrix, indicating which vertices are seeds.");
+                    return;
+                }
+            }
+            for(var r of matrix) {
+                if(r.length !== matrix[0].length) {
+                    window.alert("Invalid input. Each row must have the same length.");
+                    return;
+                }
+            };
+            if(matrix.length !== matrix[0].length + 1) {
+                window.alert("Invalid input. The adjacency matrix must be a square matrix.");
+                return;
+            }
+            for(i = 0; i < matrix.length - 1; i++) {
+                for(var j = 0; j < matrix.length - 1; j++) {
+                    if(matrix[i + 1][j] !== '0' && matrix[i + 1][j] !== '1') {
+                        window.alert("Invalid input. Every entry in the adjacency matrix must be 1 or 0, indicating the presence or absence of an edge, respectively.");
+                        return;
+                    }
+                }
+            }
+            for(i = 0; i < matrix.length - 1; i++) {
+                for(j = 0; j < i; j++) {
+                    if(matrix[i + 1][j] !== matrix[j + 1][i]) {
+                        window.alert("Invalid input. The adjacency matrix must be symmetric, defining an undirected graph.");
+                        return;
+                    }
+                }
+            }
+            for(i = 0; i < matrix.length - 1; i++) {
+                if(matrix[i + 1][i] !== '0') {
+                    if(matrix[i + 1][i] !== '0') {
+                        window.alert("Invalid input. The adjacency matrix must have 0's on the diagonal, defining a simple graph.");
+                        return;
+                    }
+                }
+            }
+
+            // Create a graph according to the adjacency matrix.
+            const graph = new Graph();
+            for(i = 0; i < matrix.length - 1; i++) {
+                graph.addVertex(i);
+                if(matrix[0][i] === '+') {
+                    graph.activateVertex(i);
+                }
+                for(j = 0; j < i; j++) {
+                    if(matrix[i + 1][j] === '1') {
+                        graph.addEdge(i, j);
+                    }
+                }
+            }
+            this.setState({graph: graph, forceData: graph.getGraphData()});
+        };
+        reader.readAsText(file);
+    }
 
   getMinContagiousSet = () => {
     this.state.graph.findMinimalContagiousSet(2)
@@ -69,7 +140,10 @@ class ForceGraph extends React.Component{
                   <br/>
                   <br/>
                   <h3>GRAPH</h3>
-                  <Button color = "Primary" variant="outlined" component="label">Upload Adjacency Matrix<input type="file" accept=".csv" hidden /></Button>
+                  <Button color = "Primary" variant="outlined" component="label">
+                      Upload Adjacency Matrix
+                      <input id="uploadAdjacencyMatrix" type="file" accept=".csv" onChange={this.readAdjacencyMatrix} hidden />
+                  </Button>
                   <br/>
                   <br/>
                   <Divider variant = "middle" color = "Secondary"/>
