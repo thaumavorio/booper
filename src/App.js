887aeeece00e51816476f64d6e49a44d29973442
ekshej <abalasubramanian@wpi.edu>
2021-01-19 17:34:06 -0500

    added basic router stuff, main app accessible at /study

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import React from "react";
 import ForceGraph from "./components/ForceGraph.js";
 import { RemoveScrollBar } from "react-remove-scroll-bar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <RemoveScrollBar />
-      <ForceGraph />
-    </div>
+    <Router>
+      <div className="App">
+        <RemoveScrollBar />
+        <Switch>
+          <Route path="/study">
+            <ForceGraph />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
