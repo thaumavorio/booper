cbeb93a5113213f014d8277f15198a0f4b877fc6
Connor Anderson <canderson@thaumavor.io>
2021-02-25 01:25:09 -0500

    Add copyright headers

--- src/classes/Graph.js
+++ src/classes/Graph.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Booper, a bootstrap percolation visualization tool.
+ * Copyright (C) 2020-2021 Connor Anderson <canderson@thaumavor.io>, Akshaj
+ * Balasubramanian <bakshaj99@gmail.com>, Henry Poskanzer <hposkanzer@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 export default class Graph {
   constructor() {
     this.adj = new Map();
